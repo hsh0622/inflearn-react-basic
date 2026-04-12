a4958ce379fd4094617a54eec51c108186865159
한승환 <coderrornotfound@gmail.com>
2026-04-13 07:23:54 +0900

    Create App.jsx

---
+++ section05/src/App.jsx
@@ -0,0 +1,18 @@
+// 간단한 회원가입 폼
+// 1. 이름
+// 2. 생년월일
+// 3. 국적
+// 4. 자기소개
+import "./App.css";
+import Register from "./components/Register";
+import HookExam from "./components/HookExam";
+
+function App() {
+  return (
+    <>
+      <HookExam />
+    </>
+  );
+}
+
+export default App;
